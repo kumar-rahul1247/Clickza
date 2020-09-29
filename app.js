cb216910aa0934ee8ffd0c7a2af4596132cf2614
kumar-rahul1247 <kumar.rahul1247@gmail.com>
2020-09-29 23:43:18 +0530

    Added /* for navigating to Index.htm from any url

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static('clickza/build'))
 
     const path = require('path')
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'clickza', 'index.html'))
     })
 }
